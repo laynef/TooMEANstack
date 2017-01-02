4249102adf5620e16fb9959bc2ef8530cf886159
laynefaler <laynefaler@gmail.com>
2017-01-02 10:41:55 -0800

    minor changes

--- project/temp/webpack.config.js
+++ project/temp/webpack.config.js
@@ -7,10 +7,22 @@ module.exports = {
   entry: {
     'app': './app/main.ts',
     'polyfills': [
-      'core-js/es6',
+      'core-js/es6/symbol',
+      'core-js/es6/object',
+      'core-js/es6/function',
+      'core-js/es6/parse-int',
+      'core-js/es6/parse-float',
+      'core-js/es6/number',
+      'core-js/es6/math',
+      'core-js/es6/string',
+      'core-js/es6/date',
+      'core-js/es6/array',
+      'core-js/es6/regexp',
+      'core-js/es6/map',
+      'core-js/es6/set',
+      'core-js/es6/reflect',
       'core-js/es7/reflect',
-      'zone.js/dist/zone',
-      'zone.js/dist/long-stack-trace-zone'
+      'zone.js/dist/zone'
     ]
   },
   output: {
